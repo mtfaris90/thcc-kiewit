f9348b371728bbc4fee0779aea815b332319be0e
Matthew Faris <mtfaris90@gmail.com>
2021-02-01 13:18:23 -0500

    corrected filtering and searching functionality

--- src/EmployeeList.js
+++ src/EmployeeList.js
@@ -5,30 +5,25 @@ import employee_data from "./data";
 const EmployeeList = ({ selection, dept, age }) => {
   const [employees, setEmployees] = useState(employee_data);
   useEffect(() => {
-    setEmployees(employee_data);
+    let copy = employee_data.slice();
     if (selection !== "") {
-      setEmployees(
-        employees.filter((item) =>
-          item.name.toLowerCase().includes(selection.toLowerCase())
-        )
+      copy = copy.filter((item) =>
+        item.name.toLowerCase().includes(selection.toLowerCase())
       );
     }
     if (dept !== "all") {
-      setEmployees(
-        employees.filter((item) => item.department.toLowerCase() === dept)
-      );
+      copy = copy.filter((item) => item.department.toLowerCase() === dept);
     }
     if (age !== "all") {
       if (age === "u30") {
-        setEmployees(employees.filter((item) => item.age < 30));
+        copy = copy.filter((item) => item.age < 30);
       } else if (age === "u40") {
-        setEmployees(
-          employees.filter((item) => item.age >= 30 && item.age <= 40)
-        );
+        copy = copy.filter((item) => item.age >= 30 && item.age <= 40);
       } else if (age === "o40") {
-        setEmployees(employees.filter((item) => item.age >= 40));
+        copy = copy.filter((item) => item.age >= 40);
       }
     }
+    setEmployees(copy);
   }, [selection, dept, age]);
   return (
     <div className="list">
